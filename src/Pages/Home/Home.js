a964952de7a3830d0eb821eaa21eda3e2db90d80
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-08-10 11:04:03 +0600

    2nd navbar added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navber from '../Navber/Navber';
+import Navber2 from '../Navber/Navber2';
 
 const Home = () => {
     return (
-        <div>
+        <div className='container'>
+            <Navber2></Navber2>
             <Navber></Navber>
         </div>
     );
