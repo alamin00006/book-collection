88831f5170c80c903f259da94ab6645bbd38ae8e
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-08-18 12:20:02 +0600

    card responsive

--- src/Pages/Products/Product.js
+++ src/Pages/Products/Product.js
@@ -4,13 +4,15 @@ import '../Products/Product.css'
 const Product = ({product}) => {
     const{_id, name, description, picture, quantity, price, suppliyerName, userEmail} = product;
     return (
-      <div className='col-lg-3 col-md-6 col-sm-12'>
-          <div class="card" style={{width: '15rem'}}>
+      <div>
+          <div>
+        <div className='d-flex flex-column'>
         <img src={picture} class="card-img-top" alt="..."/>
         <div class="card-body">
           <h5 class="card-title">জি.মাওলা BPSC MCQ Question Banke</h5>
           <p class="card-text"></p>
           <a href="#" class="btn btn-primary">Details</a>
+        </div>
         </div>
       </div>
       </div>
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useProduct from '../../Hooks/UseProduct';
 import Product from './Product';
+import '../Products/Product.css'
 
 const Products = () => {
 
@@ -9,7 +10,7 @@ const Products = () => {
         <div className='container'>
             <h1 className='mt-5'>গত সপ্তাহের বেষ্ট সেলার বই</h1>
             
-       <div className='row gy-5 my-card'>
+       <div className='my-card-main my-card'>
        {
             myProducts.slice(0,5).map( product => <Product product={product}></Product>) 
         }
