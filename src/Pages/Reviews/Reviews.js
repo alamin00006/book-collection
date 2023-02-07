d7aaf32e9a2b96b8aaa33cce902fbed56915e824
Mohammad Al Amin <alaminbamna08@gmail.com>
2023-02-07 12:38:18 +0600

    review part working

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { FaStar } from "react-icons/fa";
 import axios from 'axios';
 import { useQuery } from 'react-query';
+import { StarIcon } from '@heroicons/react/24/solid'
+import './Reviews.css'
 const colors = {
     orange: "#FFBA5A",
     grey: "#a9a9a9"
@@ -11,6 +13,7 @@ const Reviews = ({singleProduct3}) => {
     const [currentValue, setCurrentValue] = useState(0);
     const [hoverValue, setHoverValue] = useState(undefined);
     const [comment, setComment] = useState('');
+    const [name, setName] = useState('');
     const stars = Array(5).fill(0)
 
     const [reviews, setReview] = useState([]);
@@ -33,7 +36,7 @@ const { isLoading, refetch} = useQuery(['',singleProduct3 ], () => fetch(`http:/
   
 }))
 const productReviews = reviews.filter(pReview =>pReview.forProduct===singleProduct3?._id)
-console.log(productReviews)
+// console.log(productReviews)
     const handleClick = value => {
       setCurrentValue(value)
       // console.log(value)
@@ -51,10 +54,11 @@ console.log(productReviews)
     const handleReviewSubmit = async() =>{
     const reviewData = {
       forProduct:singleProduct3._id,
-      rating:currentValue,
-      comment:comment
+      rating:currentValue||hoverValue,
+      comment:comment,
+      name:name
     }
-    // console.log(reviewData)
+    console.log(reviewData)
       try{
         const data = await axios.post('http://localhost:5000/api/v1/review',reviewData);
         
@@ -71,14 +75,54 @@ console.log(productReviews)
       // e.target.reset()
     }
 
-
+    refetch()
 
     return (
        <div>
         {
-          productReviews.map(pReview => <div>
+          productReviews.map(pReview => <div key={pReview._id}>
             <span>
-                {pReview.rating}
+            {pReview?.rating===5?
+            <>
+                 <StarIcon className='starIcon-5'/>
+                 <StarIcon className='starIcon-5'/>
+                 <StarIcon className='starIcon-5'/>
+                 <StarIcon className='starIcon-5'/>
+                 <StarIcon className='starIcon-5'/>
+            </>:''}
+            {pReview?.rating===4?
+            <>
+                 <StarIcon className='starIcon-4'/>
+                 <StarIcon className='starIcon-4'/>
+                 <StarIcon className='starIcon-4'/>
+                 <StarIcon className='starIcon-4'/>
+                 <StarIcon className='starIcon-4-count'/>
+            </>:''}
+            {pReview?.rating===3?
+            <>
+                 <StarIcon className='starIcon-3'/>
+                 <StarIcon className='starIcon-3'/>
+                 <StarIcon className='starIcon-3'/>
+                 <StarIcon className='starIcon-3-count'/>
+                 <StarIcon className='starIcon-3-count'/>
+            </>:''}
+            {pReview?.rating===2?
+            <>
+                 <StarIcon className='starIcon-2'/>
+                 <StarIcon className='starIcon-2'/>
+                 <StarIcon className='starIcon-2-count'/>
+                 <StarIcon className='starIcon-2-count'/>
+                 <StarIcon className='starIcon-2-count'/>
+            </>:''}
+            {pReview?.rating===1?
+            <>
+                 <StarIcon className='starIcon-1'/>
+                 <StarIcon className='starIcon-1-count'/>
+                 <StarIcon className='starIcon-1-count'/>
+                 <StarIcon className='starIcon-1-count'/>
+                 <StarIcon className='starIcon-1-count'/>
+            </>:''}
+
             </span>
             <span>
                 {pReview.comment}
@@ -103,16 +147,26 @@ console.log(productReviews)
                   )
                 })
               }
-        <textarea onChange={(e)=>setComment(e.target.value)}
-        placeholder="What's your experience?"
-        style={styles.textarea}
-      />
+    <div>
+       <div>
+           <input type="text" onChange={(e)=>setName(e.target.value)} placeholder="Your Name" required/>
+       </div>
+         <div>
+            <textarea onChange={(e)=>setComment(e.target.value)}
+            placeholder="What's your experience?"
+            style={styles.textarea}
+          />
+         </div>
+         <div>
+            <button onClick={handleReviewSubmit}
+            style={styles.button}
+          >
+            Submit
+          </button>
+         </div>
+    </div>
 
-      <button onClick={handleReviewSubmit}
-        style={styles.button}
-      >
-        Submit
-      </button>
+     
               </div>
        </div>
     );
@@ -123,18 +177,17 @@ const styles ={
       flexDirection: "row",
     },
     textarea: {
-        border: "1px solid #a9a9a9",
-        borderRadius: 5,
-        padding: 10,
-        margin: "20px 0",
-        minHeight: 100,
-        width: 300
+        // border: "1px solid #a9a9a9",
+        // borderRadius: 5,
+        // padding: 10,
+        // margin: "20px 0",
+        // minHeight: 100,
+        // width: 300
       },
       button: {
         border: "1px solid #a9a9a9",
         borderRadius: 5,
-        width: 300,
-        padding: 10,
+       
       } 
   } 
 export default Reviews;
